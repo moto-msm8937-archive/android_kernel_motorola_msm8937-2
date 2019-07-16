3222422792c90b777a334f2d2b88a81471b63437
Wiktor Strzębała <wiktorek140@tlen.pl>
2019-07-16 12:32:46 +0200

    ANDROID: sdcardfs: Don't use OVERRIDE_CRED macro (v2, mising earlier) The macro hides some control flow, making it easier to run into bugs.
    bug: 111642636
    
    Change-Id: I37ec207c277d97c4e7f1e8381bc9ae743ad78435
    Reported-by: Jann Horn <jannh@google.com>
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Git-commit: 016fd2c5ea189d05487bdd0a492466ceecb4da28
    Git-repo: https://android.googlesource.com/kernel/common/
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>
    
    Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>

--- fs/sdcardfs/inode.c
+++ fs/sdcardfs/inode.c
@@ -897,7 +897,10 @@ static ssize_t sdcardfs_getxattr(struct dentry *dentry, const char *name,
 	const struct cred *saved_cred = NULL;
 
 	/* save current_cred and override it */
-	OVERRIDE_CRED(SDCARDFS_SB(dentry->d_sb), saved_cred, SDCARDFS_I(inode));
+	saved_cred = override_fsids(SDCARDFS_SB(dentry->d_sb),
+						SDCARDFS_I(inode)->data);
+	if (!saved_cred)
+		return -ENOMEM;
 	sdcardfs_get_lower_path(dentry, &lower_path);
 
 	lower_dentry = lower_path.dentry;
@@ -905,7 +908,7 @@ static ssize_t sdcardfs_getxattr(struct dentry *dentry, const char *name,
 	err = vfs_getxattr(lower_dentry, name, value, size);
 
 	sdcardfs_put_lower_path(dentry, &lower_path);
-	REVERT_CRED(saved_cred);
+	revert_fsids(saved_cred);
 	return err;
 }
 
